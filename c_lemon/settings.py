9bf530cc98efed21215db31475acd54776a51846
Adeel <engr.adeelkhan2002@gmail.com>
2023-08-27 17:07:55 +0500

    new changes

--- c_lemon/settings.py
+++ c_lemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'resturant',
+    'restaurant',
     'rest_framework',
 
 ]
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'c_lemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Capston',
+        'USER' :'root',
+        'PASSWORD':'AdeelKhan@00@',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'OPTIONS': { 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" }
         
     }
 }
