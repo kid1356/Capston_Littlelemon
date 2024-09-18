f8a4e0337f7b4e74339f4da417b6389bee9ef5a7
Muhammad Adeel <125259150+kid1356@users.noreply.github.com>
2024-09-18 23:22:47 +0500

    Update settings.py

--- c_lemon/settings.py
+++ c_lemon/settings.py
@@ -80,17 +80,10 @@ WSGI_APPLICATION = 'c_lemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Capston',
-        'USER' :'root',
-        'PASSWORD':'AdeelKhan@00@',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
-        'OPTIONS': { 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" }
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
